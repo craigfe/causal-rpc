3f07475faf6346fbe022856a655a234475168c5c
Craig Ferguson <cf443@cam.ac.uk>
2018-10-22 20:39:43 +0100

    feat(server): implement random server naming

--- src/server.ml
+++ src/server.ml
@@ -7,9 +7,24 @@ let config = Irmin_git.config ~bare:true "/tmp/irmin/test"
 (* Commit information *)
 let info author fmt = Irmin_unix.info ~author:author fmt
 
-let generate_random () = "rand" (* TODO: implement *)
+let generate_random_name () =
+	Random.self_init (); (* Initialise the random number generator *)
+    let rand_char_code () = match Random.int(26+26+10) with
+        n when n < 26 -> int_of_char 'a' + n
+      | n when n < 26 + 26 -> int_of_char 'A' + n - 26
+      | n -> int_of_char '0' + n - 26 - 26 in
 
-let run ?(name=generate_random()) port =
+    let rand_string _ = rand_char_code ()
+        |> char_of_int
+        |> (String.make 1) in
+
+    Array.init 8 rand_string
+        |> Array.to_list
+        |> List.cons "server--"
+        |> String.concat ""
+        |> fun x -> print_endline ("No server name supplied. Generated random name " ^ x ^ "."); x
+
+let run ?(name=generate_random_name()) port =
 
     (* Open the repo *)
     Store.Repo.v config >>=
@@ -18,7 +33,7 @@ let run ?(name=generate_random()) port =
     Store.master >>= fun t ->
 
     (* Set key "foo/bar" to "testing 123" *)
-    Store.set t ~info:(info name "Updating foo/bar") ["foo"; "bar"] (string_of_int port) >>= fun () ->
+    Store.set t ~info:(info name "Updating foo/bar") ["foo"; "bar"] (name ^ " running on " ^ (string_of_int port)) >>= fun () ->
 
     (* Get key "foo/bar" and print it to stdout *)
     Store.get t ["foo"; "bar"] >|=
