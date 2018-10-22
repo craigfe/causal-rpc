bdfa57ff88a4263979b9109a090f4ad86dd218e2
Craig Ferguson <cf443@cam.ac.uk>
2018-10-22 22:32:02 +0100

    style(server): minor code formatting

--- src/server.ml
+++ src/server.ml
@@ -11,9 +11,9 @@ let generate_random_name ?(length=8) () =
 	Random.self_init (); (* Initialise the random number generator *)
 
     let rand_char_code () = match Random.int(26+26+10) with
-        n when n < 26 -> int_of_char 'a' + n
+        n when n < 26      -> int_of_char 'a' + n
       | n when n < 26 + 26 -> int_of_char 'A' + n - 26
-      | n -> int_of_char '0' + n - 26 - 26 in
+      | n                  -> int_of_char '0' + n - 26 - 26 in
 
     let rand_string _ = rand_char_code ()
         |> char_of_int
