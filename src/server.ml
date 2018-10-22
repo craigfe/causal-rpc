aab4ff1337d297ac1b4ddf7951e638d5b31d12d7
Craig Ferguson <cf443@cam.ac.uk>
2018-10-22 21:11:36 +0100

    feat(server): parameterise random name length

--- src/server.ml
+++ src/server.ml
@@ -7,8 +7,9 @@ let config = Irmin_git.config ~bare:true "/tmp/irmin/test"
 (* Commit information *)
 let info author fmt = Irmin_unix.info ~author:author fmt
 
-let generate_random_name () =
+let generate_random_name ?(length=8) () =
 	Random.self_init (); (* Initialise the random number generator *)
+
     let rand_char_code () = match Random.int(26+26+10) with
         n when n < 26 -> int_of_char 'a' + n
       | n when n < 26 + 26 -> int_of_char 'A' + n - 26
@@ -18,13 +19,13 @@ let generate_random_name () =
         |> char_of_int
         |> (String.make 1) in
 
-    Array.init 8 rand_string
+    Array.init length rand_string
         |> Array.to_list
         |> List.cons "server--"
         |> String.concat ""
         |> fun x -> print_endline ("No server name supplied. Generated random name " ^ x ^ "."); x
 
-let run ?(name=generate_random_name()) port =
+let run ?(name=generate_random_name ()) port =
 
     (* Open the repo *)
     Store.Repo.v config >>=
