8c8be8e3127313ae59c8bb8e61400fcabdeea91b
Craig Ferguson <cf443@cam.ac.uk>
2018-10-30 00:09:27 +0000

    style(test): refactor test cases

--- test/test.ml
+++ test/test.ml
@@ -1,12 +1,24 @@
 open Trace_rpc
 
 let test_marshal () =
-  let open Marshal in
-  Rpc.make (Id.id_of_int 123) ["Some"; "example"; "params"] None
-  |> Fmt.strf "%a\n" Rpc.pp
-  |> Alcotest.(check string) "Simple serialisation" "A"
+  let open Marshal in begin
+    let rpc_test_case (description, rpc, expected_value) =
+      rpc
+      |> Fmt.strf "%a" Rpc.pp
+      |> Alcotest.(check string) description expected_value
 
-let print m = Fmt.pr "%a\n%!"  m
+    in let test_cases =
+         [ ( "Simple serialisation without results"
+           , Rpc.make (Id.id_of_int 123) ["Some"; "example"; "params"] None
+           , "{\"id\":123,\"params\":[\"Some\",\"example\",\"params\"]}")
+
+         ; ( "Simple serialisation with results"
+           , Rpc.make (Id.id_of_int 0) [] (Some "result in here")
+           , "{\"id\":0,\"params\":[],\"result\":\"result in here\"}")
+         ]
+
+    in List.iter rpc_test_case test_cases
+  end
 
 let test_base () =
   Alcotest.(check string) "The random name function begins with `server--`"
