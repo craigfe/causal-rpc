e198ef8b667b313b10013a6b7e2c5262010f8a09
Craig Ferguson <cf443@cam.ac.uk>
2018-10-31 23:22:34 +0000

    feat(worker): hand-implement git file:// protocol

--- src/worker/main.ml
+++ src/worker/main.ml
@@ -2,19 +2,30 @@ open Lwt.Infix
 
 module Store = Irmin_unix.Git.FS.KV(Irmin.Contents.String)
 module Sync = Irmin.Sync(Store)
+
+let server_config = Irmin_git.config ~bare:true "/tmp/irmin/server"
 let config = Irmin_git.config ~bare:true "/tmp/irmin/worker"
 
-let upstream =
-  (* This works *)
-  (* Irmin.remote_uri "git@github.com:CraigFe/trace-rpc.git" *)
+(** Get an Irmin.store at a local or remote URI. *)
+let upstream uri =
+  if String.sub uri 0 7 = "file://" then
+    let dir = String.sub uri 7 (String.length uri - 7) in
+    let lwt =
+      Irmin_git.config ~bare:true dir
+      |> Store.Repo.v
+      >>= Store.master
+      >|= Irmin.remote_store (module Store)
+    in Lwt_main.run lwt
 
-  (* This does not *)
-  Irmin.remote_uri "file:///tmp/irmin/server"
+  else
+    Irmin.remote_uri uri
 
 let test () =
+  let upstr = upstream "file:///tmp/irmin/server" in
+
   Store.Repo.v config
   >>= Store.master
-  >>= fun t  -> Sync.pull_exn t upstream `Set
+  >>= fun t  -> Sync.pull_exn t upstr `Set
   >>= fun () -> Store.get t ["test_file.md"]
   >|= fun r  -> Printf.printf "%s\n%!" r
 
