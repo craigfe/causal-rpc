e2250e0e6bd72f6e26aba835ebcf8b85a0805dad
Craig Ferguson <cf443@cam.ac.uk>
2018-10-31 14:18:15 +0000

    feat: remove failed attempt at RPC marshalling

--- src/rpc.ml
+++
@@ -1,9 +0,0 @@
-
-module Version = struct
-  type t = int
-  let compare a b = compare a b
-end
-
-type response = {contents: string}
-type call = {name: string; params: string list}
-let call name params = {name; params}
--- src/rpc.mli
+++
@@ -1,9 +0,0 @@
-
-module Version : sig
-  type t = int
-  val compare : t -> t -> int
-end
-
-type response = {contents: string}
-type call = {name: string; params: string list}
-val call : string -> string list -> call
