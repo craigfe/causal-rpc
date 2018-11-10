c927239d0e37f9576d87d045994bd9e74839effe
Craig Ferguson <cf443@cam.ac.uk>
2018-11-10 00:53:48 +0000

    feat(integer_increment): use new intmap api

--- examples/integer_increment/server.ml
+++ examples/integer_increment/server.ml
@@ -1,13 +1,15 @@
-
 open Trace_rpc
 
 let main () =
   let open Intmap in
   IntMap.empty ~directory:"/tmp/irmin/integer_increment" ()
-  |> IntMap.add "a" 1
-  |> IntMap.add "b" 10
-  |> IntMap.add "c" 100
-  |> IntMap.map (* Perform an integer increment *)
+  |> IntMap.add "a" (Int64.of_int 1)
+  |> IntMap.add "b" (Int64.of_int 10)
+  |> IntMap.add "c" (Int64.of_int 100)
+  |> IntMap.map "double" (* Perform an integer increment *)
   |> fun _ -> ()
 
-let () = main ()
+let () =
+  Logs.set_reporter (Logs_fmt.reporter ());
+  Logs.set_level (Some Logs.Info);
+  main ()
