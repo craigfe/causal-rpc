1476aa163162f8d8e3487fb81fc8d25cb9cef782
Craig Ferguson <cf443@cam.ac.uk>
2019-03-15 21:46:24 +0000

    feat(map): major refactor to use wakeup threads rather than polling

--- src/trace-rpc/map.ml
+++ src/trace-rpc/map.ml
@@ -202,120 +202,137 @@ module Make (Store: Store.S)
     | Ok () -> Lwt.return_unit
     | Error we -> Lwt.fail @@ Store.Store_error we
 
+  (* Thread that fails after f *)
+  let timeout_thread f =
+    Store.B.sleep f
+    >|= (fun () -> MProf.Trace.label "ServerTimeoutThread")
+    >>= fun () -> Lwt.fail Exceptions.Timeout
+
+  let callback () =
+    let (thread, wait) = MProf.Trace.named_task "ServerWaitForResponse" in
+    (wait, thread)
+
+  (* The callback to be executed when we detect changes to the repository.
+     Note: here we don't explicitly signal the workers that are available for a
+     map, so we have to watch for any changes in the repo and then filter within
+     the callback. *)
+  let irmin_map_watch_callback
+      ~(repo: IrminStore.repo)
+      ~(branch: IrminStore.t)
+      (callback: unit Lwt.u)
+      (map_name: string) =
+
+    (* This is the value to be passed to Irmin *)
+    fun (br_name: IrminStore.branch) (_: IrminStore.commit Irmin.diff) ->
+
+    (* Here we assume that all branches that don't start with 'map--', 'client' or 'master'
+       are worker branches for this map request. In future this may not always be the case.
+       TODO: create a better namespacing scheme for the different branch types *)
+    Logs_lwt.info (fun m -> m "Callback triggered on branch %s" br_name)
+    >>= fun () -> if String.equal br_name map_name then Lwt.return_unit
+
+    else if not (String.sub br_name  0 5 |> String.equal "map--")
+         && not (String.sub br_name 0 6 |> String.equal "client")
+         && not (String.equal "master" br_name) then begin
+
+      (* Check the job queue on the branch *)
+      IrminStore.of_branch repo br_name
+      >>= Store.JobQueue.peek_opt
+      >>= (function
+          | Some (Job.MapJob m) when String.equal map_name m ->
+
+            Logs_lwt.debug (fun m -> m "Resetting count due to activity on branch %s" br_name)
+
+            (* Merge the work from this branch *)
+            >>= fun () -> IrminStore.merge_with_branch branch
+              ~info:(Store.B.make_info ~author:"map" "Merged work from %s into %s" br_name map_name) br_name
+
+            >>= Helpers.handle_merge_conflict br_name map_name
+
+            (* If the task queue is now empty, trigger the map callback *)
+            >>= fun () -> task_queue_is_empty branch
+            >>= (function
+                | true ->
+                  Logs_lwt.err (fun m -> m "TRIGGERING THE CALLBACK.\n br_name: %s \n map_name: %s" br_name map_name)
+                  >>= fun () -> Lwt.return (Lwt.wakeup callback ()) (* We are done *)
+                | false -> Lwt.return_unit) (* Wait for the next callback *)
+
+          | Some j -> Logs_lwt.warn
+                        (fun m -> m "Woke up due to submitted work for a job %a, but the currently executing job is %s"
+                            Job.pp j map_name)
+
+          | None -> Logs_lwt.warn
+                      (fun m -> m "Woke up due to branch %s, but there is no job on this branch" br_name))
+
+    end else
+      Logs_lwt.warn (fun m -> m "Woke up due to an irrelevant branch %s when waiting for work on %s" br_name map_name)
+
   let map ?(timeout=5.0) operation params m =
 
     let l = m.local in
 
-    (* Generate a new unique branch name for the map *)
-    let rec unique_name_gen () =
-      Lwt.wrap (fun () -> "map--" ^ Helpers.generate_rand_string ~length:8 ())
-      >>= fun map_name -> IrminStore.Branch.mem (IrminStore.repo l) map_name
-      >>= fun exists -> if exists then unique_name_gen () else Lwt.return map_name
-    in unique_name_gen ()
+    (* If the map is currently empty, don't bother issuing an operation *)
+    is_empty m
+    >>= function
+    | true -> Logs_lwt.info (fun m -> m "Issued map operation on an empty map. Returning immediately.")
+      >|= fun () -> m
 
-    >>= fun map_name -> Logs_lwt.app (fun m -> m "Map operation issued. Branch name %s" map_name)
+    | false ->
 
-    (* Push the job onto the job queue *)
-    >>= fun () -> Store.JobQueue.push (Job.MapJob map_name) l
+      (* Generate a new unique branch name for the map *)
+      let rec unique_name_gen () =
+        Lwt.wrap (fun () -> "map--" ^ Helpers.generate_rand_string ~length:8 ())
+        >>= fun map_name -> IrminStore.Branch.mem (IrminStore.repo l) map_name
+        >>= fun exists -> if exists then unique_name_gen () else Lwt.return map_name
+      in unique_name_gen ()
 
-    (* Create a new branch to isolate the operation *)
-    >>= fun () -> IrminStore.clone ~src:l ~dst:map_name
-    >>= fun branch -> IrminStore.merge_with_branch l
-      ~info:(Store.B.make_info ~author:"map" "Merged") IrminStore.Branch.master
-    >>= Helpers.handle_merge_conflict IrminStore.Branch.master map_name
+      >>= fun map_name -> Logs_lwt.app (fun m -> m "Map operation issued. Branch name %s" map_name)
 
-    (* Generate and commit the task queue *)
-    >>= fun () -> generate_task_queue operation params m
-    >>= fun tq -> set_task_queue tq branch
+      (* Push the job onto the job queue *)
+      >>= fun () -> Store.JobQueue.push (Job.MapJob map_name) l
 
-    (* Wait for the task queue to be empty *)
-    >>= fun () ->
+      (* Create a new branch to isolate the operation *)
+      >>= fun () -> IrminStore.clone ~src:l ~dst:map_name
+      >>= fun branch -> IrminStore.merge_with_branch l
+        ~info:(Store.B.make_info ~author:"map" "Merged") IrminStore.Branch.master
+      >>= Helpers.handle_merge_conflict IrminStore.Branch.master map_name
 
-    let inactivity_count = ref 0.0 in
-    let sleep_interval = ref 0.01 in
+      (* Generate and commit the task queue *)
+      >>= fun () -> generate_task_queue operation params m
+      >>= fun tq -> set_task_queue tq branch
 
-    (* The callback to be executed when we detect changes to the repository.
-       Note: here we don't explicitly signal the workers that are available for a
-       map, so we have to watch for any changes in the repo and then filter within
-       the callback. *)
-    let watch_callback (br_name: IrminStore.branch) (_: IrminStore.commit Irmin.diff) =
+      (* Wait for the task queue to be empty *)
+      >|= callback
+      >>= fun (wait, callback_thread) ->
 
-      (* Here we assume that all branches that don't start with 'map--' are worker branches
-         for this map request. In future this may not always be the case *)
-      if String.equal br_name map_name then Lwt.return_unit
-      else if not (String.sub br_name  0 5 |> String.equal "map--") then begin
+      let repo = IrminStore.repo l in
+      IrminStore.Branch.watch_all repo (irmin_map_watch_callback ~repo ~branch wait map_name)
 
-        IrminStore.of_branch (IrminStore.repo l) br_name
-        >>= Store.JobQueue.peek_opt
-        >>= fun job -> (match job with
-            | Some (Job.MapJob m) when String.equal map_name m ->
+      >>= fun watch -> Lwt.pick [(callback_thread >>= fun () -> IrminStore.unwatch watch); timeout_thread timeout]
 
-              Logs_lwt.debug (fun m -> m "Resetting count due to activity on branch %s" br_name)
-              >|= (fun () -> inactivity_count := 0.0; sleep_interval := !sleep_interval /. 2.)
+      (* Sanity check that the callback was only triggered after the queue is empty *)
+      >>= fun () -> task_queue_is_empty branch
+      >|= (fun empty -> assert empty)
 
-              (* Merge the work from this branch *)
-              >>= fun () -> IrminStore.merge_with_branch branch
-                ~info:(Store.B.make_info ~author:"map" "Merged work from %s into %s" br_name map_name) br_name
+      >>= (fun () -> Logs_lwt.info @@ fun m -> m "All operations complete on %s. Merging with the master branch" map_name)
 
-              >>= Helpers.handle_merge_conflict br_name map_name
+      (* Merge the map branch into master *)
+      >>= fun () -> IrminStore.merge_with_branch l
+        ~info:(Store.B.make_info ~author: "map" "Job %s complete" map_name) map_name
+      >>= Helpers.handle_merge_conflict map_name IrminStore.Branch.master
 
-            | Some j -> Logs_lwt.warn
-                          (fun m -> m "Woke up due to submitted work for a job %a, but the currently executing job is %s"
-                              Job.pp j map_name)
+      (* Remove the job from the job queue *)
+      >>= fun () -> Store.JobQueue.pop l
 
-            | None -> Lwt.fail @@ E.Protocol_error (Printf.sprintf "Received work on branch %s, but there is no job on this branch" br_name))
+      (* For now, we only ever perform one map at once. Eventually, the job queue
+         will need to be cleverer to avoid popping off the wrong job here *)
+      >>= fun j -> (match j with
+          | Ok (Job.MapJob m) when m = map_name -> Lwt.return_unit
+          | Ok j -> Lwt.fail_with (Fmt.strf "Expected to pop %a, but actually popped %a" Job.pp (Job.MapJob map_name) Job.pp j)
+          | Error msg -> Lwt.fail_with (Fmt.strf "Error <%s> when attempting to remove %a from the job queue" msg Job.pp (Job.MapJob map_name)))
 
-      end else
-        Logs_lwt.warn (fun m -> m "Woke up due to an irrelevant branch %s when waiting for work on %s" br_name map_name)
-
-    in
-
-    IrminStore.Branch.watch_all (IrminStore.repo l) watch_callback
-
-    >>= fun watch -> Logs_lwt.app (fun m -> m "Waiting for the task queue to be empty, with timeout %f" timeout)
-    >>= fun () ->
-
-    let rec inner () =
-
-      task_queue_is_empty branch
-      >>= fun is_empty -> if is_empty then (* we are done *)
-        IrminStore.unwatch watch
-
-      else if !inactivity_count >= timeout then (* we have been waiting for too long *)
-        Logs_lwt.app (fun m -> m "Inactivity count: %f" (!inactivity_count))
-        >>= fun () -> Lwt.fail E.Timeout
-
-      else (* we will wait for a bit *)
-        task_queue_size branch
-        >>= fun tq_size -> Logs_lwt.app (fun m -> m "Sleeping for a time of %f, with an inactivity count of %f. %d tasks remaining"
-                                            !sleep_interval !inactivity_count tq_size)
-        >>= fun () -> Store.B.sleep (!sleep_interval)
-        >|= (fun () ->
-            inactivity_count := !inactivity_count +. !sleep_interval;
-            sleep_interval := !sleep_interval *. 2.)
-        >>= inner
-
-    in inner ()
-
-    >>= (fun () -> Logs_lwt.info @@ fun m -> m "All operations complete on %s. Merging with the master branch" map_name)
-
-    (* Merge the map branch into master *)
-    >>= fun () -> IrminStore.merge_with_branch l
-      ~info:(Store.B.make_info ~author: "map" "Job %s complete" map_name) map_name
-    >>= Helpers.handle_merge_conflict map_name IrminStore.Branch.master
-
-    (* Remove the job from the job queue *)
-    >>= fun () -> Store.JobQueue.pop l
-
-    (* For now, we only ever perform one map at once. Eventually, the job queue
-       will need to be cleverer to avoid popping off the wrong job here *)
-    >>= fun j -> (match j with
-        | Ok (Job.MapJob m) when m = map_name -> Lwt.return_unit
-        | Ok j -> Lwt.fail_with (Fmt.strf "Expected to pop %a, but actually popped %a" Job.pp (Job.MapJob map_name) Job.pp j)
-        | Error msg -> Lwt.fail_with (Fmt.strf "Error <%s> when attempting to remove %a from the job queue" msg Job.pp (Job.MapJob map_name)))
-
-    >>= (fun _ -> Logs_lwt.app @@ fun m -> m "Map operation complete. Branch name %s" map_name)
-    >|= fun () -> m
+      >>= (fun _ -> Logs_lwt.app @@ fun m -> m "Map operation complete. Branch name %s" map_name)
+      >|= fun () -> m
 
   (* This is the main server process *)
   let start m =
@@ -324,42 +341,42 @@ module Make (Store: Store.S)
 
     (* The callback wakes up a thread on our queue *)
     let watch_callback (br_name: IrminStore.branch) (_: IrminStore.commit Irmin.diff) =
+      let () = MProf.Trace.label "ServerWatchCallback" in
       IrminStore.of_branch (IrminStore.repo l) br_name
       >>= fun local -> Store.JobQueue.pop local
-      >>= fun result -> (match result with
-      | Ok job -> Lwt.return job
-      | Error msg -> Lwt.fail_with (Fmt.strf "Error <%s> when attempting to pop from job queue on branch %s" msg br_name))
+      >>= function
+      | Error msg -> Logs_lwt.err (fun m -> m "Error <%s> when attempting to pop from job queue on branch %s" msg br_name)
+      | Ok Job.MapJob _ -> Lwt.return_unit
+      | Ok Job.Rpc (t, remote) ->
 
-      >>= fun job -> (match job with
-          | Job.Rpc (t, remote) ->
-            let boxed_mi () = (match I.find_operation_opt t.name Impl.api with
-                | Some operation -> operation
-                | None -> invalid_arg "Operation not found") in
+        let boxed_mi () = (match I.find_operation_opt t.name Impl.api with
+            | Some operation -> operation
+            | None -> invalid_arg "Operation not found") in
 
-            (* Found a pending RPC task *)
-            IrminStore.get local ["val"]
-            >>= (fun v -> match v with Value v -> Lwt.return v | _ -> raise E.Internal_type_error)
-            >>= fun old_val -> Ex.execute_task (boxed_mi ()) t.params old_val
+        (* Found a pending RPC task *)
+        IrminStore.get local ["val"]
+        >>= (function Value v -> Lwt.return v | _ -> raise E.Internal_type_error)
+        >>= fun old_val -> let () = MProf.Trace.label "ServerTaskExecute" in
+        Ex.execute_task (boxed_mi ()) t.params old_val
 
-            (* Commit value to store *)
-            >>= fun new_val -> IrminStore.set local ["val"]
-              ~info:(Store.B.make_info ~author:"server" "%a" pp_task t) (Value new_val)
+        (* Commit value to store *)
+        >>= fun new_val -> IrminStore.set local ["val"]
+          ~info:(Store.B.make_info ~author:"server" "%a" pp_task t) (Value new_val)
 
-            >>= fun res -> (match res with
-                | Ok () -> Lwt.return_unit
-                | Error se -> Lwt.fail @@ Store.Store_error se)
+        >>= (function
+            | Ok () -> Lwt.return_unit
+            | Error se -> Lwt.fail @@ Store.Store_error se)
 
-            (* Merge into master *)
-            (* TODO *)
+        (* Merge into master *)
+        (* TODO *)
 
-            (* Push back to the client *)
-            >>= fun () -> Store.upstream ~uri:remote ~branch:br_name
-            >>= fun remote -> Sync.push local remote
-            >>= fun res -> (match res with
-                | Ok () -> Lwt.return_unit
-                | Error pe -> Lwt.fail @@ Store.Push_error pe)
+        (* Push back to the client *)
+        >>= fun () -> Store.upstream ~uri:remote ~branch:br_name
+        >>= fun remote -> Sync.push local remote
+        >>= (function
+            | Ok () -> Lwt.return_unit
+            | Error pe -> Lwt.fail @@ Store.Push_error pe)
 
-          | Job.MapJob _ -> Lwt.return_unit)
 
     in let _ = IrminStore.Branch.watch_all (IrminStore.repo l) watch_callback in
     Lwt.return_unit
